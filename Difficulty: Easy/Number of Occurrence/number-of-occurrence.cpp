10edcfcc8654532b9206544b0336950fda7d153c
Gururaj SK <gururajkarpurmath@gmail.com>
2026-08-27 18:26:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of Occurrence/number-of-occurrence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        unordered_map<int,int> mp;
+        for( int i = 0 ; i< arr.size() ; i++){
+            mp[arr[i]]++;
+        }
+        return mp[target];
+    }
+};
