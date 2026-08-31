3972c5a73953f7818418ecb5d83b91ca5196c390
Gururaj SK <gururajkarpurmath@gmail.com>
2026-08-31 10:47:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Meetings in One Room/maximum-meetings-in-one-room.cpp
+++ Difficulty: Medium/Maximum Meetings in One Room/maximum-meetings-in-one-room.cpp
@@ -2,27 +2,28 @@ class Solution {
   public:
     vector<int> maxMeetings(vector<int>& s, vector<int>& f) {
         vector<vector<int>> meetings;
-
-        for (int i = 0; i < s.size(); i++)
-            meetings.push_back({f[i], s[i], i + 1});
-
-        sort(meetings.begin(), meetings.end(), [](auto &a, auto &b) {
-            return a[0] < b[0];
+        vector<int> result;
+        for(int i =0 ; i< s.size();i++){
+            meetings.push_back({s[i],f[i],i+1});
+        }
+        sort(meetings.begin(), meetings.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
         });
-
-        vector<int> ans;
-
-        int lastFinish = -1;
-
-        for (auto &m : meetings) {
-            if (m[1] > lastFinish) {
-                ans.push_back(m[2]);
-                lastFinish = m[0];
+        result.push_back(meetings[0][2]);
+        int last_end = meetings[0][1];
+        
+        for(int i =1 ;  i< meetings.size();i++){
+            if(last_end < meetings[i][0]){
+                result.push_back(meetings[i][2]);
+                last_end = meetings[i][1];
             }
         }
-
-        sort(ans.begin(), ans.end());
-
-        return ans;
+        sort(result.begin(),result.end());
+        
+        return result;
+        
+        
+        
+        
     }
 };
